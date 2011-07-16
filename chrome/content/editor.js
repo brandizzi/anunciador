cdb6c8f9027d6629dbd86fc5ff34e316c3400911
Adam Victor Nazareth Brandizzi <brandizzi@gmail.com>
2011-07-16 16:39:52 -0300

    New AnunciadorEditor object

--- chrome/content/editor.js
+++ chrome/content/editor.js
@@ -1,19 +1,21 @@
-var editorController = {
+var AnunciadorEditor = {
     editor : null,
 
     onLoad : function() {
-        editorController.editor = document.getElementById('editor');
-        editorController.editor.contentDocument.designMode = 'on';
+        AnunciadorEditor.editor = document.getElementById('editor');
+        AnunciadorEditor.editor.contentDocument.designMode = 'on';
+        AnunciadorEditor.contentWindow = AnunciadorEditor.editor.contentWindow;
+        AnunciadorEditor.commandManager = AnunciadorEditor.editor.commandManager;
     },
 
     onBoldButtonCommand : function() {
-        editorController.editor.commandManager.doCommand('cmd_bold', {}, editorController.editor.contentWindow);
+        AnunciadorEditor.commandManager.doCommand('cmd_bold', {}, AnunciadorEditor.contentWindow);
     },
     onItalicButtonCommand : function() {
-        editorController.editor.commandManager.doCommand('cmd_italic', {}, editorController.editor.contentWindow);
+        AnunciadorEditor.commandManager.doCommand('cmd_italic', {}, AnunciadorEditor.contentWindow);
     },
     onUnderlineButtonCommand : function() {
-        editorController.editor.commandManager.doCommand('cmd_underline', {}, editorController.editor.contentWindow);
+        AnunciadorEditor.commandManager.doCommand('cmd_underline', {}, AnunciadorEditor.contentWindow);
     },
     onColorSelectCommand : function() {
         var colorMenuList = document.getElementById('color');
@@ -21,9 +23,9 @@ var editorController = {
         var color = colorMenuList.selectedItem.value;
         var commandParams = Components.classes['@mozilla.org/embedcomp/command-params;1'].getService(Components.interfaces.nsICommandParams);
         commandParams.setCStringValue("state_attribute", color);
-        editorController.editor.commandManager.doCommand(
+        AnunciadorEditor.commandManager.doCommand(
                 'cmd_fontColor', commandParams, 
-                editorController.editor.contentWindow);
+                AnunciadorEditor.contentWindow);
     }
 }
-window.addEventListener("load", editorController.onLoad, false);
+window.addEventListener("load", AnunciadorEditor.onLoad, false);
